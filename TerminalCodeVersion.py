c9d9dd552e2380a5b6652f3fdf7d0966f48b40d8
Pranav Eranki <pranav.eranki@gmail.com>
2018-08-03 18:48:18 -0700

    Adding TryCatch for stock code

--- TerminalCodeVersion.py
+++ TerminalCodeVersion.py
@@ -23,16 +23,30 @@ def getKey():
     else:
         quandl.ApiConfig.api_key = input(key_prompt)
 
+def StockCodeWorks(name):
+    try:
+        data = quandl.get("WIKI/" + name)
+    except:
+        return False;
+    return True;
 
 def getNameAndForecast():
-    name = input("What is the abbreviation for the Stock you wish to view? ")
+    while(True):
+        name = input("What is the abbreviation for the Stock you wish to view? ")
+        if (StockCodeWorks(name)):
+            break
+        else:
+            print("The stock code you entered does not exist in the Quandl Api. Please Try Again.")
+            
+    data = quandl.get("WIKI/" + name)
+    
     forecast = input("How many days into the future would you like to predict? ")
     forecast = int(forecast)
+    
     return name,forecast
 
 
 def getData(name):
-
     data = quandl.get("WIKI/" + name)
     return data
 
@@ -124,6 +138,8 @@ def main():
     name,forecast = getNameAndForecast()
     printWorking()
     predict(name,forecast)
-
+'''
 if __name__ == "__main__":
     main()
+'''
+getNameAndForecast()
